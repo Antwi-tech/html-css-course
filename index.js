639a1275c4ce8551484f21f39b729e3361747787
Nana Antwiwaa <nanaafuaantwiwwaa624@gmail.com>
2023-11-04 00:40:43 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+//let name= "Antwiwaa";
+//console.log(name);
+
+let person={
+    age: 30,
+    name:" Antwiwaa",
+    yearOfBirth: 2005
+}
+console.log(person)
+
+function greet(firstName , lastName){
+    console.log("My first name is "+" "+firstName+" "+"and my last name is "+lastName);
+}
+greet("Nana","Conduah");
